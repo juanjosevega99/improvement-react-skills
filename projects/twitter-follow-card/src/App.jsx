15d82cb0ce308d327a2c4a5a20a507775e10f9c6
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2023-02-20 22:31:59 -0500

    format: lint the TwitterFollowCard

--- projects/twitter-follow-card/src/App.jsx
+++ projects/twitter-follow-card/src/App.jsx
@@ -18,7 +18,13 @@ function App () {
   return (
     <section className='App'>
       {users.map(({ userName, name, isFollowing }) => (
-        <TwitterFollowCard key={userName} userName={userName} initialIsFollowing={isFollowing}>{name}</TwitterFollowCard>
+        <TwitterFollowCard
+          key={userName}
+          userName={userName}
+          initialIsFollowing={isFollowing}
+        >
+          {name}
+        </TwitterFollowCard>
       ))}
     </section>
   )
