7f538b77709d8ae0695fc961241f77571db8bd9b
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2023-02-20 22:05:44 -0500

    feat: render the username

--- projects/twitter-follow-card/src/App.jsx
+++ projects/twitter-follow-card/src/App.jsx
@@ -1,7 +1,26 @@
 import './App.css'
 
+const users = [
+  {
+    userName: 'juanjosevega99',
+    name: 'Juan Jose Vega',
+    isFollowing: true
+  },
+  {
+    userName: 'paulaceronj',
+    name: 'Paula Ceron.',
+    isFollowing: false
+  }
+]
+
 function App () {
-  return <div className='App'>xd</div>
+  return (
+    <section className='App'>
+      {users.map(({ userName, name }) => (
+        <h1 key={userName}>{name}</h1>
+      ))}
+    </section>
+  )
 }
 
 export default App
