e9bd2fa4a7d6e774580b156eb807f93c06615709
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2023-02-26 21:17:31 -0500

    chore: fetch of the api to show images

--- projects/technical-interview-fetch-random-event/src/App.jsx
+++ projects/technical-interview-fetch-random-event/src/App.jsx
@@ -1,11 +1,37 @@
 import './App.css'
+import { useState, useEffect } from 'react'
 
-function App () {
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+// const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firsWord}?size=50`
+
+export function App () {
+  const [fact, setFact] = useState()
+  const [imageUrl, setImageUrl] = useState()
+
+  useEffect(() => {
+    fetch(CAT_ENDPOINT_RANDOM_FACT)
+      .then(res => res.json())
+      .then(data => {
+        const { fact } = data
+        setFact(fact)
+      })
+
+    const threeFirstWords = fact.split(' ', 3)
+    console.log('🚀 ~ file: App.jsx:16 ~ useEffect ~ threeFirstWords:', threeFirstWords)
+
+    fetch(`https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`)
+      .then(res => res.json())
+      .then(response => {
+        const { url } = response
+        setImageUrl(url)
+      })
+  }, [])
+
   return (
     <main>
       <h1>App of cats</h1>
+      {fact && <p>{fact}</p>}
+      {imageUrl && <img src={imageUrl} alt='' />}
     </main>
   )
 }
-
-export default App
