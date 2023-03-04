6778fcf83f8ce27b6c317b89cc69bb5047864541
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2023-03-03 22:13:04 -0500

    refactor: use the function getRandomFact

--- projects/technical-interview-fetch-random-event/src/App.jsx
+++ projects/technical-interview-fetch-random-event/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import { useState, useEffect } from 'react'
+import { getRandomFact } from '../services/fact'
 
-const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
 // const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firsWord}?size=50`
 const CAT_PREFIX_IMAGE_URL = 'https://cataas.com'
 
@@ -10,21 +10,20 @@ export function App () {
   const [imageUrl, setImageUrl] = useState()
 
   useEffect(() => {
-    fetch(CAT_ENDPOINT_RANDOM_FACT)
-      .then(res => res.json())
-      .then(data => {
-        const { fact } = data
-        setFact(fact)
-
-        const threeFirstWords = fact.split(' ', 3).join(' ')
-        fetch(`https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`)
-          .then(res => res.json())
-          .then(response => {
-            const { url } = response
-            setImageUrl(url)
-          })
-      })
+    getRandomFact().then(setFact)
   }, [])
+
+  useEffect(() => {
+    if (!fact) return
+
+    const threeFirstWords = fact.split(' ', 3).join(' ')
+    fetch(`https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`)
+      .then(res => res.json())
+      .then(response => {
+        const { url } = response
+        setImageUrl(url)
+      })
+  }, [fact])
 
   return (
     <main>
