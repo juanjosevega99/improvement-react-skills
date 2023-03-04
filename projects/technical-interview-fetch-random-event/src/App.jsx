49c9ec6c824e1027844840e2e0b3f3768d30a6a4
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2023-03-04 16:12:18 -0500

    refactor: add custom hook

--- projects/technical-interview-fetch-random-event/src/App.jsx
+++ projects/technical-interview-fetch-random-event/src/App.jsx
@@ -5,13 +5,8 @@ import { getRandomFact } from '../services/fact'
 // const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firsWord}?size=50`
 const CAT_PREFIX_IMAGE_URL = 'https://cataas.com'
 
-export function App () {
-  const [fact, setFact] = useState()
+function useCatImage ({ fact }) {
   const [imageUrl, setImageUrl] = useState()
-
-  useEffect(() => {
-    getRandomFact().then(setFact)
-  }, [])
 
   useEffect(() => {
     if (!fact) return
@@ -25,9 +20,27 @@ export function App () {
       })
   }, [fact])
 
+  return { imageUrl }
+}
+
+export function App () {
+  const [fact, setFact] = useState()
+  const { imageUrl } = useCatImage({ fact })
+
+  useEffect(() => {
+    // getRandomFact().then(newFact => setFact(newFact))
+    getRandomFact().then(setFact)
+  }, [])
+
+  const handleClick = async () => {
+    const newFact = await getRandomFact()
+    setFact(newFact)
+  }
+
   return (
     <main>
       <h1>App of cats</h1>
+      <button onClick={handleClick}>Get new fact</button>
       {fact && <p>{fact}</p>}
       {imageUrl && <img src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`} alt='' />}
     </main>
