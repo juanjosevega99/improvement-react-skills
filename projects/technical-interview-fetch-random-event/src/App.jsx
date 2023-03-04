5c9705f586a055e9c36878d250eb632c30fac6e3
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2023-03-04 16:28:51 -0500

    refactor: new custom hook useCatFact

--- projects/technical-interview-fetch-random-event/src/App.jsx
+++ projects/technical-interview-fetch-random-event/src/App.jsx
@@ -1,40 +1,29 @@
 import './App.css'
 import { useState, useEffect } from 'react'
 import { getRandomFact } from '../services/fact'
+import { useCatImage } from '../hooks/useCatImage'
 
-// const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firsWord}?size=50`
 const CAT_PREFIX_IMAGE_URL = 'https://cataas.com'
 
-function useCatImage ({ fact }) {
-  const [imageUrl, setImageUrl] = useState()
+const useCatFact = () => {
+  const [fact, setFact] = useState()
 
-  useEffect(() => {
-    if (!fact) return
+  const refreshFact = () => {
+    // getRandomFact().then(newFact => setFact(newFact))
+    getRandomFact().then(setFact)
+  }
 
-    const threeFirstWords = fact.split(' ', 3).join(' ')
-    fetch(`https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`)
-      .then(res => res.json())
-      .then(response => {
-        const { url } = response
-        setImageUrl(url)
-      })
-  }, [fact])
+  useEffect(refreshFact, [])
 
-  return { imageUrl }
+  return { fact, refreshFact }
 }
 
 export function App () {
-  const [fact, setFact] = useState()
+  const { fact, refreshFact } = useCatFact()
   const { imageUrl } = useCatImage({ fact })
 
-  useEffect(() => {
-    // getRandomFact().then(newFact => setFact(newFact))
-    getRandomFact().then(setFact)
-  }, [])
-
   const handleClick = async () => {
-    const newFact = await getRandomFact()
-    setFact(newFact)
+    refreshFact()
   }
 
   return (
